077ff63642e1d623c03b6a6e36ea94bf99572754
Daniel Savage <djm87@wildcats.unh.edu>
2018-12-06 10:03:26 -0700

    Typo

--- InstallRepo2Path.m
+++ InstallRepo2Path.m
@@ -1,7 +1,7 @@
 %Use: in matlab or octave navigate to the git repository and execute this script. 
 %This installs the git repository to the startup paths for Matlab or Octave. 
 %All scripts can then be executed from any directory in you system, allowing you to
-%avoid haveing to copy the scripts around for different refinement directories.
+%avoid having to copy the scripts around for different refinement directories.
 P=genpath(pwd)
 addpath(P)
 savepath
